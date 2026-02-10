758ac1e726c48619cd0c25f4a5e4bf55bf648a1f
Edvinas Rimkus <s062930@ad.viko.lt>
2026-02-10 13:23:23 +0200

    first commit

---
+++ edvinasrimkus1.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string vardas = "Edvinas";
+    string pavarde = "Rimkus";
+    int amzius = 20;
+    string grupe = "Is25";
+    int kursas = 2;
+    string studijuprograma = "programu sistemos";
+
+    cout << "studento info" << endl;
+    cout << "vardas: " << vardas << endl;
+    cout << "pavarde: " << pavarde << endl;
+    cout << "amzius: " << amzius << endl;
+    cout << "grupe: " << grupe << endl;
+    cout << "kursas: " << kursas << endl;
+    cout << "studiju programa: " << studijuprograma << endl << endl;
+
+    return 0;
+}
